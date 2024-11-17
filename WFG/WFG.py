70d172ef798ff3155572ffd026cf941bc51cd20d
Jfortner0 <Jackiefort@icloud.com>
2024-11-17 11:26:14 -0800

    WFG folder and file

---
+++ WFG/WFG.py
@@ -0,0 +1,23 @@
+
+
+# Reference: https://www.geeksforgeeks.org/wait-for-graph-deadlock-detection-in-distributed-system/
+
+def create_graph():
+    return {} # empty graph
+
+def create_edge(): # Adding edges to simulate processes waiting on each other
+    ...
+
+def delete_edge():
+    ...
+
+def dfs(): # Perform DFS
+    ...
+
+def detect_cycle(): # We will use DFS approach 
+    ...
+
+def main():
+    ...
+
+main()
